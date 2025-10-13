c93581b59f6f9c63cd30fbb213bd46e848a023af
Anon <gabebuel@gmail.com>
2025-10-13 02:12:34 +0000

    set up score functionality

--- hello_world_app/urls.py
+++ hello_world_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.hello, name='hello'),
     path('example/', views.index, name='index'), 
     path('example/about/', views.about, name='about'),
-    path('score', views.score_view, name='score_view'), 
+    path('score/', views.score_view, name='score_view'), 
     path('score/edit/<int:score_id>/', views.edit_score, name='edit_score'), 
     path('score/delete/<int:score_id>/', views.delete_score, name='delete_score'),
 ]
--- hello_world_app/views.py
+++ hello_world_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import Score
+from .forms import ScoreForm
 
 def hello(request):
     return HttpResponse("Hello World!!!!")
@@ -9,4 +11,35 @@ def index(request):
 
 def about(request): 
     return render(request, 'about_hello.html')
+
+def score_view(request):
+    scores = Score.objects.all()
+    
+    if request.method == "POST":
+        form = ScoreForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('score_view')
+    else:
+        form = ScoreForm()
+
+    return render(request, 'score_list.html', {'form': form, 'scores': scores})
+
+def edit_score(request, score_id): 
+    score = get_object_or_404(Score, id=score_id)
+
+    if request.method == "POST":
+        form = ScoreForm(request.POST, instance=score)
+        if form.is_valid():
+            form.save()
+            return redirect('score_view')
+    else:
+        form = ScoreForm(instance=score)
+    
+    return render(request, 'score_edit.html', {'form': form, 'score': score})
+
+def delete_score(request, score_id):
+    score = get_object_or_404(Score, id=score_id)
+    score.delete()
+    return redirect('score_view')
 # Create your views here.
